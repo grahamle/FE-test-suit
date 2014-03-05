827192bc5ddbdaa94b6d85e5cc1800b394bb5c6c
yinhuangle <yinhuangle@gmail.com>
2014-03-05 12:52:36 +0800

    作业预览系统-alpha.0.1 添加ng的索引，部署app的路由选择，分为4个模式：默认（列表模式）-->单个查看-->单个编辑-->新建模式

---
+++ angular-hw/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var app = angular.module('hw', ['hw.directives','hw.services']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      resolve: {
+      	homeworks: function(MultiHwLoader) {
+          return MultiHwLoader();
+      	}
+      },
+      controller: 'ListCtrl'
+    }).when('/edit/:hwId', {
+      resolve: {
+      	homework: function(HwLoader) {
+      	  return HwLoader();
+      	}
+      },
+      controller: 'EditCtrl',
+      templateUrl: '/views/hwForm.html'
+    }).when('/view/:hwId', {
+      resolve: {
+      	homework: function(HwLoader) {
+      	  return HwLoader();
+      	}
+      },
+      controller: 'ViewCtrl',
+      templateUrl: '/views/viewHw.html'
+    }).when('/new', {
+      controller: 'NewCtrl',
+      templateUrl: '/views/hwForm.html'
+    }).otherwise({
+      redirectTo: '/'
+    });
+}]);
