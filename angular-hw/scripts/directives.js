d230adcd7b9724767c5c4f5c2e1d0d009f83abf0
yinhuangle <yinhuangle@gmail.com>
2014-03-05 14:28:13 +0800

    作业预览系统-alpha.0.5 添加了一个页面加载时候用来显示的指令；模块加载ok；但是ng-route的路由选择显然在本地情况下无法完成，需要搭建一个server来承载http地址变换引起的路由跳转（本地因为是file协议，显然是不行的）

---
+++ angular-hw/scripts/directives.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var directives = angular.module('hw.directives', []);
+
+directives.directive('loadbar', ['$rootScope', function($rootScope) {
+  return {
+  	link: function(scope, iElem, iAttrs) {
+  	  iElem.addClass('hide');
+  	  $rootScope.$on('$routeChangeStart', function() {
+  	  	iElem.removeClass('hide');
+  	  });
+  	  $rootScope.$on('$routeChangeSuccess', function() {
+  	  	iElem.addClass('hide');
+  	  });
+  	}
+  };
+}]);
